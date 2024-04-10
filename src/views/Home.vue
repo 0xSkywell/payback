f1705c11882122556cd9a33c67ca15d58c57e316
skywell <719779468@qq.com>
2024-04-10 14:09:16 +0800

    administrator address test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -262,7 +262,8 @@
                         this.showMessage('Please connect your wallet', 'error');
                         return;
                     }
-                    if (!["0x2a40c0a18e3121b8fbdf34876f99a4b1e0a00154", "0xb278038cce5ab79efbd65dcee217497a73e7c623", "0x646592183ff25a0c44f09896a384004778f831ed"].includes(walletAddress.toLowerCase())) {
+                    if (!["0x2a40c0a18e3121b8fbdf34876f99a4b1e0a00154", "0xb278038cce5ab79efbd65dcee217497a73e7c623",
+                      "0x646592183ff25a0c44f09896a384004778f831ed", "0xa5f46d60f4f08f11a5495f8c1011537718e188fe"].includes(walletAddress.toLowerCase())) {
                         this.showMessage('Please select the administrator address to return the money', 'error');
                         return;
                     }
